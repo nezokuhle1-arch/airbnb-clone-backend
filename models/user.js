bb42883cb323158800fa989da954d712e78a5cfc
nezzyaura <nezokuhle1@gmail.com>
2026-08-30 19:21:02 +0200

    feat: added comments on user schema.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
+/**
+ * User Schema notes
+ * User is a schema that represents a user of the application.
+ * It contains the username, email, password, and role.
+ * @typedef {Object} User
+ * @property {string} username - The username of the user.
+ * @property {string} email - The email of the user.
+ * @property {string} password - The password of the user.
+ * @property {string} role - The role of the user.
+ */
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
