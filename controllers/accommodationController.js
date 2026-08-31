acfaade486ab051f484180ba4f30f1938d12c85f
nezzyaura <nezokuhle1@gmail.com>
2026-08-31 21:02:30 +0200

    feat: add createAccommodation and getAllAccommodations controllers

--- controllers/accommodationController.js
+++ controllers/accommodationController.js
@@ -13,4 +13,13 @@ const createAccommodation = async (req, res) => {
     }
 };
 
-module.exports = { createAccommodation };
+const getAllAccommodations = async (req, res) => {
+    try {
+        const accommodations = await Accommodation.find().populate('host', 'username email');
+        res.status(200).json(accommodations);
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to get accommodations', error: error.message});
+    }
+};
+
+module.exports = { createAccommodation, getAllAccommodations };
